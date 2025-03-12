544733f52b7dd5783c92197ef342f9f1acc403b9
Grecia Delgado <greciadm2596@gmail.com>
2025-03-12 16:10:30 -0500

    MongoBD connected

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('AuthService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('MongoBD connected');
+  }
+}
